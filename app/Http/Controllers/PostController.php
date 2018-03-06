554b2a763b8833e76b8b97ac0bb98ff25505f18e
Jurica Šoštarić <bugificiran@gmail.com>
2018-03-06 13:38:47 +0100

    validating and inserting data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -37,7 +38,22 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validate the data
+        $this->validate($request, array(
+			"title" => "required|max:255",
+			"body" => "required"
+		));
+        
+        //store in db
+        $post = new Post;
+		
+		$post->title = $request->title;
+		$post->body = $request->body;
+		
+		$post->save();
+        
+        //redirect to another page
+        return redirect()->route("posts.show", $post->id);
     }
 
     /**
